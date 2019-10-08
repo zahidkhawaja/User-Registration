1a873b04e89d063ac5bd270df84a600bf84fd076
Zahid Khawaja <zahidkhawaja@icloud.com>
2019-10-08 17:41:06 -0400

    Correct checkIfStrongPassword function to return false if password contains Password or password

--- enforce.js
+++ enforce.js
@@ -1,9 +1,7 @@
 function checkIfStrongPassword() {
-
     let firstPasswordValue = document.getElementById("firstPassword").value;
     let usernameValue = document.getElementById("username").value;
-
-    if (firstPasswordValue.includes(usernameValue) || firstPasswordValue.includes("firstPassword") || firstPasswordValue.length < 8) {
+    if (firstPasswordValue.includes(usernameValue) || firstPasswordValue.includes("Password") || firstPasswordValue.includes("password") || firstPasswordValue.length < 8) {
         return false;
     } else {
         return true;
@@ -11,11 +9,7 @@ function checkIfStrongPassword() {
 }
 
 function checkIfEmailValid() {
-
-
     let emailValue = document.getElementById("email").value;
-
-
     if (emailValue.includes("@") && emailValue.length >= 6) {
         return true;
     } else {
@@ -24,13 +18,10 @@ function checkIfEmailValid() {
 }
 
 function registerAccount() {
-
     let usernameValue = document.getElementById("username").value;
     let emailValue = document.getElementById("email").value;
     let firstPasswordValue = document.getElementById("firstPassword").value;
     let secondPasswordValue = document.getElementById("secondPassword").value;
-
-
     if (checkIfStrongPassword() == true && checkIfEmailValid() == true && firstPasswordValue === secondPasswordValue) {
         document.getElementById("result").innerHTML = `Account successfully registered. Please check ${emailValue} to confirm.`;
     } else if (firstPasswordValue != secondPasswordValue) {
